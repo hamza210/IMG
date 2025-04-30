29d35f10452c7b49bfbb814198442e0a90a91ba5
hamza <hamzadkhan150@gmail.com>
2025-04-30 20:44:35 +0530

    basic setup

---
+++ img-app/src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwcP5Vn-QbKBIQ9pQKKStkRz_ZWSESgmg",
+  authDomain: "img-app-8720b.firebaseapp.com",
+  projectId: "img-app-8720b",
+  storageBucket: "img-app-8720b.firebasestorage.app",
+  messagingSenderId: "508494693971",
+  appId: "1:508494693971:web:a7e6feb67bd8822e45e8a6"
+};
+
+initializeApp(firebaseConfig);
